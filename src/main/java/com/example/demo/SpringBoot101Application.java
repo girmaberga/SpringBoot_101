d8a075486240ac2006d9d468897649fd2119e1ef
girmaberga <GBTC440019UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-08-20 15:09:22 -0400

    ex01

---
+++ src/main/java/com/example/demo/SpringBoot101Application.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot101Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBoot101Application.class, args);
+        System.out.println("Hello welcome to Spring boot lesson 1");
+    }
+
+}
